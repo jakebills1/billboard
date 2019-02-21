1ce071859669c1c878c5e845adad7ea28cb76ca2
Jake Bills <jakebills1@gmail.com>
2019-02-21 14:25:53 -0700

    initial commit; add chart model and controller and routes

---
+++ app/controllers/charts_controller.rb
@@ -0,0 +1,53 @@
+class ChartsController < ApplicationController
+  before_action :set_chart, only: [:show, :update, :edit, :destroy]
+
+  def index
+    @charts = Chart.all
+  end
+
+  def show
+  end
+
+  def new
+    @chart = Chart.new
+    render partial: 'form'
+  end
+
+  def edit
+    render partial: 'form'
+  end
+
+  def create
+    @chart = Chart.new(chart_params)
+
+    if @chart.save
+      redirect_to charts_path
+    else
+      render :new
+    end
+
+  end
+
+  def update
+    if @chart.update(chart_params)
+      redirect_to @chart
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @chart.destroy
+    redirect_to charts_path
+  end
+
+  private
+    def chart_params
+      params.require(:chart).permit(:name)
+    end
+
+    def set_chart
+      @chart = Chart.find(params[:id])
+    end
+
+end
