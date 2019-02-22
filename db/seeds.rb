1bcceeaf91dfe5fa3323ad555b2ec9fe186222d6
Jake Bills <jakebills1@gmail.com>
2019-02-22 16:01:32 -0700

    pre-seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+chart_array = [nil, 9, 10, 11]
+50.times do
+    Song.create(artist_id: 3, title: Faker::Movies::GratefulDead.song, genre: "jamband", chart_id: chart_array.sample)
+end
+50.times do
+    Song.create(artist_id: 4, title: Faker::Music::Phish.song, genre: "jamband", chart_id: chart_array.sample)
+end
+50.times do
+    Song.create(artist_id: 5, title: Faker::Music::UmphreysMcgee.song, genre: "jamband", chart_id: chart_array.sample)
+end
+
