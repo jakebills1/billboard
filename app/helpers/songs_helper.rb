baab23059d49651e21542cc7c5a68f137d1ec66a
Jake Bills <jakebills1@gmail.com>
2019-02-23 13:07:06 -0700

    add styling for artist index

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,2 +1,10 @@
 module SongsHelper
+    def generate_song_lengths
+        first_digits = ["2", "3", "4"]
+        second_digits = ["0", "1", "2", "3", "4", "5"]
+        third_digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        return first_digits.sample + ":" + second_digits.sample + third_digits.sample
+    end
+
+
 end
